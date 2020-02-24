68cadc8785df3aabc3c62eb928416021f064c783
Jake Hemstad <jhemstad@nvidia.com>
2020-02-24 11:15:21 -0600

    Add device.

--- example.cu
+++ example.cu
@@ -5,6 +5,7 @@
 #include <thrust/for_each.h>
 
 struct foo {
+  __device__
   int increment(int i) const { return i + 1; }
 };
 
@@ -12,8 +13,6 @@ TEST_CASE("Mutable device lambda test") {
   auto begin = thrust::make_counting_iterator(0);
   auto end = thrust::make_counting_iterator(100);
   thrust::device_vector<int> ints(100);
-
   foo f;
-
-  thrust::for_each(begin, end, [f] __device__(int i) { f.increment(i); });
+  thrust::for_each(thrust::device, begin, end, [f] __device__ (int i) { f.increment(i); });
 }
